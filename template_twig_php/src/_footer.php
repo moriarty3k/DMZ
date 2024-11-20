3737efdf6349800012cc9dbd4dd49dd5471e67e8
Hoang Ngoc Phu Dat <hdat1305@gmail.com>
2024-11-20 18:05:07 +0700

    add more challenge

---
+++ template_twig_php/src/_footer.php
@@ -0,0 +1,14 @@
+
+        </div>
+    
+        <div class="clearer"><span></span></div>
+
+    </div>
+
+    <div class="footer">&copy; 2024 Cookie Arena</div>
+
+</div>
+
+</body>
+
+</html>
---
+++ template_twig_php/src/home.php
@@ -0,0 +1,31 @@
+ 
+
+<p>
+
+    Subcribe để nhận thông báo mới nhất.
+    <form method='post' action=''>
+        <div class="form-group"> 
+            <input placeholder="Điền email của bạn (admin@example.com)" name="email" size=50></input> <button class="btn btn-default" type="submit" name='submit'>Submit</button>
+       </div> 
+    </form>
+
+<?php
+include('vendor/twig/twig/lib/Twig/Autoloader.php');
+if (isset($_POST['email'])) {
+    $email=$_POST['email'];
+
+    Twig_Autoloader::register();
+    try {
+        $loader = new Twig_Loader_String();
+        $twig = new Twig_Environment($loader);
+
+        $result= $twig->render("Chào mừng {$email}. Chúng tôi sẽ liên hệ lại trong thời gian sớm nhất.");
+        echo $result;
+
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
+
+?>
+</p>
